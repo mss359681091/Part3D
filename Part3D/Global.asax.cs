d70867f60bef24974360f6003c0eb8b03adf26a3
赛赛 <@IDEA-PC>
2016-06-21 15:06:34 +0800

    添加jquerymobile

--- Part3D/Global.asax.cs
+++ Part3D/Global.asax.cs
@@ -15,6 +15,14 @@ namespace _3DPart
         {
             // 在应用程序启动时运行的代码
             log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(Server.MapPath("Log4Net.config")));
+
+            //定义连接字符串  
+            string conStr = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            System.Data.SqlClient.SqlDependency.Start(conStr);//启动监听服务，ps：只需启动一次  
+            System.Web.Caching.SqlCacheDependencyAdmin.EnableNotifications(conStr);//设置通知的数据库连接，ps：只需设置一次  
+            string[] str = { "dp_Advertisement", "dp_Classify", "dp_DownRecord", "dp_Links", "dp_ModelFile", "dp_Part", "dp_Standard", "dp_StandardMapping", "dp_Verification", "sysUser", "tLog" };
+            System.Web.Caching.SqlCacheDependencyAdmin.EnableTableForNotifications(conStr, str);//设置通知的数据库连接和表，ps：只需设置一次  
+
         }
 
         protected void Application_AuthenticateRequest(Object sender, EventArgs e)
@@ -25,7 +33,7 @@ namespace _3DPart
                 {
                     if (HttpContext.Current.User.Identity is FormsIdentity)
                     {
-                        FormsIdentity id =(FormsIdentity)HttpContext.Current.User.Identity;
+                        FormsIdentity id = (FormsIdentity)HttpContext.Current.User.Identity;
 
                         FormsAuthenticationTicket ticket = id.Ticket;
 
